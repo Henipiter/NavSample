ae9dc2f9ba168587306ab3477969243200d3d5d2
Henryk <henipiter@wp.pl>
2023-09-27 09:34:51 +0200

    working camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.2.3")
     implementation("androidx.camera:camera-view:1.2.3")
     implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-extensions:1.2.3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.mlkit:vision-common:17.3.0")
--- app/src/main/java/com/example/navsample/CameraFragment.kt
+++ app/src/main/java/com/example/navsample/CameraFragment.kt
@@ -1,9 +1,9 @@
 package com.example.navsample
 
-import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,19 +15,24 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
-import androidx.camera.view.LifecycleCameraController
-import androidx.camera.view.PreviewView
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.navsample.databinding.FragmentCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var cameraController: LifecycleCameraController
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
     private var capturedImage: ImageProxy? = null
 
 
@@ -44,10 +49,10 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if (!hasPermissions(requireContext())) {
-            activityResultLauncher.launch(REQUIRED_PERMISSIONS)
-        } else {
+        if (allPermissionsGranted()) {
             startCamera()
+        } else {
+            requestPermissions()
         }
 
         binding.bottomText.setOnClickListener {
@@ -58,12 +63,9 @@ class CameraFragment : Fragment() {
             }
             val action = CameraFragmentDirections.actionCameraFragmentToShopListFragment(bitmapVal)
             Navigation.findNavController(it).navigate(action)
-
         }
 
-//        capturedImage
-
-
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun imageToBitMap(imageProxy: ImageProxy): Bitmap {
@@ -76,58 +78,92 @@ class CameraFragment : Fragment() {
     }
 
     private fun takePhoto() {
-        cameraController.takePicture(
+        val imageCapture = imageCapture ?: return
+        Log.e(TAG, "OOOO")
+        imageCapture.takePicture(
             ContextCompat.getMainExecutor(requireContext()),
             object : ImageCapture.OnImageCapturedCallback() {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
                 override fun onCaptureSuccess(image: ImageProxy) {
+                    val msg = "Photo capture succeeded"
                     capturedImage = image
-
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
                 }
-
-                override fun onError(exception: ImageCaptureException) {
-                    exception.message?.let { Log.e("CAMERA", it) }
-                    Log.e("CAMERA", exception.stackTraceToString())
-                    Toast.makeText(
-                        context,
-                        "Photo not captured:" + exception.message,
-                        Toast.LENGTH_LONG
-                    ).show()
-
-                }
-            })
+            }
+        )
     }
 
     private fun startCamera() {
-        val previewView: PreviewView = binding.cameraView
-        cameraController = LifecycleCameraController(requireContext())
-        cameraController.bindToLifecycle(this)
-        cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        previewView.controller = cameraController
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraView.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
     }
 
     private val activityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
             var permissionGranted = true
             permissions.entries.forEach {
-                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
                     permissionGranted = false
             }
             if (!permissionGranted) {
-                Toast.makeText(requireContext(), "Permission request denied", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    requireContext(),
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
                 startCamera()
             }
         }
 
-    companion object {
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf(android.Manifest.permission.CAMERA).apply { }.toTypedArray()
-
-        fun hasPermissions(context: Context) = REQUIRED_PERMISSIONS.all {
-            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
-        }
-
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(android.Manifest.permission.CAMERA).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 
 }
