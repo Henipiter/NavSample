535d34f2db2df68b7ff3b92502cad5c2d2834e88
Henipiter <henipiter@wp.pl>
2023-12-30 18:27:21 +0100

    add searchable StoreDropdownAdapter

--- app/src/main/java/com/example/navsample/adapters/StoreDropdownAdapter.kt
+++ app/src/main/java/com/example/navsample/adapters/StoreDropdownAdapter.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import com.example.navsample.databinding.ArrayAdapterRowBinding
 import com.example.navsample.entities.Store
 
 class StoreDropdownAdapter(
-    ctx: Context, res: Int, private var storeList: ArrayList<Store>
-) : ArrayAdapter<Store>(ctx, res, storeList) {
+    ctx: Context, res: Int, var storeList: ArrayList<Store>
+) : ArrayAdapter<Store>(ctx, res, storeList), Filterable {
+
+    private var filteredList = ArrayList<Store>()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
@@ -20,8 +24,48 @@ class StoreDropdownAdapter(
             false
         )
         binding.root.tag = binding
-        binding.name.text = storeList[position].name
-        binding.nip.text = storeList[position].nip
+        binding.name.text = filteredList[position].name
+        binding.nip.text = filteredList[position].nip
         return binding.root
     }
+
+    override fun getCount(): Int {
+        return filteredList.size
+    }
+
+    override fun getItem(position: Int): Store {
+        return filteredList[position]
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+
+                val filteredList: List<Store>
+                val filterText = constraint?.toString()?.uppercase()
+                if (filterText.isNullOrBlank()) {
+                    filteredList = storeList
+                    filteredList.sortedBy { it.name }
+                } else {
+                    filteredList = storeList.filter { it.name.uppercase().contains(filterText) }
+                    filteredList.sortedWith(compareBy({
+                        it.name.indexOf(
+                            filterText,
+                            ignoreCase = true
+                        )
+                    }, { it.name }))
+                }
+                val results = FilterResults()
+                results.values = filteredList
+                results.count = filteredList.size
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filteredList = results?.values as? ArrayList<Store> ?: arrayListOf()
+                notifyDataSetChanged()
+            }
+        }
+    }
+
 }
