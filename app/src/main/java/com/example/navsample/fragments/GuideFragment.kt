8ea41a67bab7d03f0a0643547024f70ec141d6d6
Cezary Milewski <cezary.milewski@comarch.com>
2024-10-01 16:41:27 +0200

    delete gemini demo from guide fragment

--- app/src/main/java/com/example/navsample/fragments/GuideFragment.kt
+++ app/src/main/java/com/example/navsample/fragments/GuideFragment.kt
@@ -1,17 +1,13 @@
 package com.example.navsample.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.example.navsample.R
 import com.example.navsample.databinding.FragmentGuideMenuBinding
-import com.example.navsample.imageanalyzer.GeminiAssistant
-import kotlinx.coroutines.launch
 
 
 class GuideFragment : Fragment() {
@@ -30,16 +26,6 @@ class GuideFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val geminiAssistant = GeminiAssistant()
-        viewLifecycleOwner.lifecycleScope.launch {
-            val response = geminiAssistant.sendRequest(
-                "\"D_JZN_ZIEMNIAKI JAD\" \n" +
-                        "\"D_WAFLE_SONKO 130G\""
-            )
-            Log.i("Gemini", response ?: "EMPTY")
-        }
-
-
 
         binding.test.setOnClickListener {
             Navigation.findNavController(it)
