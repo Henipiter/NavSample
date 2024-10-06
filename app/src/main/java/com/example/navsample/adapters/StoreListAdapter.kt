733c07c84b31324f0b093668f56113b012077f54
Henipiter <henipiter@wp.pl>
2024-10-06 19:30:19 +0200

    feat: print red nip if it is incorrect

--- app/src/main/java/com/example/navsample/adapters/StoreListAdapter.kt
+++ app/src/main/java/com/example/navsample/adapters/StoreListAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.navsample.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.navsample.ItemClickListener
+import com.example.navsample.R
 import com.example.navsample.databinding.StoreRowBinding
 import com.example.navsample.entities.Store
 
@@ -27,7 +29,12 @@ class StoreListAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         this.position = position
         holder.binding.storeName.text = storeList[position].name
-        holder.binding.nip.text = storeList[position].nip
+        if (storeList[position].nip != "") {
+            holder.binding.nip.text = storeList[position].nip
+        } else {
+            holder.binding.nip.text = "---"
+
+        }
 
         holder.binding.mainLayout.setOnClickListener {
             itemClickListener.onItemClick(position)
@@ -36,8 +43,31 @@ class StoreListAdapter(
             onDelete.invoke(position)
             true
         }
+        if (!isCorrectNIP(storeList[position].nip)) {
+            holder.binding.nip.setTextColor(Color.RED)
+        } else {
+            holder.binding.nip.setTextColor(
+                context.resources.getColor(
+                    R.color.basic_text_grey,
+                    context.theme
+                )
+            )
+        }
 
 
+    }
+
+
+    private fun isCorrectNIP(valueNIP: String?): Boolean {
+        if (valueNIP == null || !Regex("""[0-9]{10}""").matches(valueNIP)) {
+            return false
+        }
+        val weight = arrayOf(6, 5, 7, 2, 3, 4, 5, 6, 7)
+        var sum = 0
+        for (i in 0..8) {
+            sum += valueNIP[i].digitToInt() * weight[i]
+        }
+        return sum % 11 == valueNIP[9].digitToInt()
     }
 
     override fun getItemCount(): Int {
